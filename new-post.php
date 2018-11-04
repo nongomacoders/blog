79cd4382466823ca2e3360f82612346e78493ed1
asusdion <dr.dtdavan@gmail.com>
2018-11-04 14:27:49 +0200

    tab padding and width adjusted

--- new-post.php
+++ new-post.php
@@ -2,7 +2,7 @@
 $myfile = fopen("index.html", "r") or die("Unable to open file!");
 $tempindexhtml=fread($myfile,filesize("index.html"));
 fclose($myfile);
-$pos_end_a=strpos($tempindexhtml, "</a>");
+$pos_end_a=strrpos($tempindexhtml, "</a>");
 $pos_to_insert=$pos_end_a+4;
 
 $string_to_insert='<div class="card">'
